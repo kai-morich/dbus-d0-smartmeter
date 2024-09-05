ab122639d5b916a4823b98e7ae7cd3dac1995649
Kai Morich <mail@kai-morich.de>
2024-09-05 20:12:01 +0000

    install/uninstall improvements
    install thru rc.local needed also for reboot as of https://github.com/victronenergy/meta-victronenergy/commit/7c45ff619fc0121da4d071e8c1158a43d9014281
    uninstallable even if only partially installed
    deleted runtime files

--- restart.sh
+++ restart.sh
@@ -3,4 +3,5 @@ set -x
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SERVICE_NAME=$(basename $SCRIPT_DIR)
 
-kill $(pgrep -f "python $SCRIPT_DIR/$SERVICE_NAME.py")
+pids=$(pgrep -f "python $SCRIPT_DIR/$SERVICE_NAME.py")
+[[ -n $pids ]] && kill $pids
